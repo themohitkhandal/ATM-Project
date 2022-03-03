d2f061b96f499ca6460a13a83bedd64f1d3bdcb2
Mohit Desktop <mohit1319khandal@gmail.com>
2022-03-03 14:25:48 +0530

    added a new account number and pin

--- src/login.java
+++ src/login.java
@@ -3,7 +3,12 @@ public class login{
         if (menu.acc_no == 123 && menu.acc_pin == 1234){
             System.out.println("Login Accepted");
             menu.functions();
-        } else {
+        }
+        else if (menu.acc_no == 258 && menu.acc_pin == 4569){
+            System.out.println("Login Accepted");
+            menu.functions();
+        }
+        else {
             System.out.println("Incorrect!");
         }
     }
